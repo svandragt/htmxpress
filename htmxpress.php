12a8e134c086ed5234cfeb0e9f4d3764d3b13fed
Sander van Dragt <sander@vandragt.com>
2024-01-09 16:44:01 +0000

    Refactor code to comply with PHP Coding Standards
    The PHP code has been refactored to adhere to PSR-1 Coding Standards. This includes adding comprehensive function docblocks for 'activate', 'deactivate', and 'bootstrap' functions, as well as correcting the structure to allow for PHP CodeSniffer's PSR1 rules. Furthermore, PHP CodeSniffer's warning for side effects has been ignored on the first line of script.

--- htmxpress.php
+++ htmxpress.php
@@ -1,16 +1,17 @@
-<?php
-/*
-Plugin Name:  HTMXpress
-Plugin URI:   https://vandragt.com
-Description:  HTMX for WordPress
-Version:      0.1
-Author:       Sander van Dragt <sander@vandragt.com>
-Author URI:   https://vandragt.com
-License:      GPL2
-License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:  htmxpress
-Domain Path:  /languages
-*/
+<?php //phpcs:ignore PSR1.Files.SideEffects.FoundWithSymbols
+
+/**
+ * Plugin Name:  HTMXpress
+ * Plugin URI:   https://vandragt.com
+ * Description:  HTMX for WordPress
+ * Version:      0.1
+ * Author:       Sander van Dragt <sander@vandragt.com>
+ * Author URI:   https://vandragt.com
+ * License:      GPL2
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:  htmxpress
+ * Domain Path:  /languages
+ */
 
 namespace HtmxPress;
 
@@ -18,20 +19,35 @@ require_once( __DIR__ . '/inc/assets.php' );
 require_once( __DIR__ . '/inc/endpoint.php' );
 require_once( __DIR__ . '/inc/template.php' );
 
-function activate() {
+/**
+ * Activate the plugin.
+ *
+ * @return void
+ */
+function activate() : void {
 	Endpoint\register();
 	flush_rewrite_rules();
 }
 
 register_activation_hook( __FILE__, __NAMESPACE__ . '\\activate' );
 
-function deactivate() {
+/**
+ * Deactivate the plugin.
+ *
+ * @return void
+ */
+function deactivate() : void {
 	flush_rewrite_rules();
 }
 
 register_deactivation_hook( __FILE__, __NAMESPACE__ . '\\deactivate' );
 
-function bootstrap() {
+/**
+ * Bootstrap the plugin.
+ *
+ * @return void
+ */
+function bootstrap() : void {
 	Endpoint\register();
 	Template\bootstrap();
 	Assets\bootstrap();
